e94e6c13f844b8c3f70419030c501457da327011
afw821 <afw821@gmail.com>
2018-12-12 06:01:40 -0500

    I added more users and images for each user to increase the number of matches

--- app/data/employees.js
+++ app/data/employees.js
@@ -1,31 +1,26 @@
-module.exports = [{
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  },{
-    "name":"Dr. Evil",
-    "photo":"http://www.bobharris.com/wp-content/uploads/2012/09/Screen-Shot-2012-09-06-at-2.41.54-PM.png",
-    "scores":[
-        4,
-        1,
-        4,
-        4,
-        3,
-        1,
-        2,
-        2,
-        4,
-        1
-      ]
-  }]
+module.exports = [
+  {
+    name: "Number 2",
+    photo:
+      "https://desu-usergeneratedcontent.xyz/mlp/image/1364/49/1364492653630.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Dr. Evil",
+    photo:
+      "http://www.bobharris.com/wp-content/uploads/2012/09/Screen-Shot-2012-09-06-at-2.41.54-PM.png",
+    scores: [4, 1, 4, 4, 3, 1, 2, 2, 4, 1]
+  },
+  {
+    name: "Scott Evil",
+    photo:
+      "https://carboncostume.com/wordpress/wp-content/uploads/2018/05/scottevil-austinpowers.jpg",
+    scores: [5, 2, 3, 5, 3, 1, 3, 2, 1, 1]
+  },
+  {
+    name: "Austin Powers",
+    photo:
+      "https://i2.wp.com/www.usmagazine.com/wp-content/uploads/2018/06/Mike-Myers-Austin-Powers.jpg?crop=0px%2C278px%2C1488px%2C841px&resize=1600%2C900&ssl=1",
+    scores: [2, 1, 5, 4, 3, 3, 2, 5, 4, 1]
+  }
+];
